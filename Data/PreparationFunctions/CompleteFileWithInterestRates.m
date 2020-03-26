add3ee866db6ce3481a03d02ad390c9a1a3a17f6
lgrigoryeva <l.v.grigoryeva@gmail.com>
2020-03-26 09:15:12 +0100

    added Tbill based interest rates
    added Tbill based interest rates

--- Data/PreparationFunctions/CompleteFileWithInterestRates.m
+++ Data/PreparationFunctions/CompleteFileWithInterestRates.m
@@ -1,8 +1,9 @@
+%%
 clear;
 load('SP500_date_prices_returns_realizedvariance_090320.mat');
 data = SP500_date_prices_returns_realizedvariance_090320;
 
-load('interestRates2001to032020.mat');
+load('interestRatesYield2001to032020.mat');
 data(5:8,:) = NaN;
 [~, T] = size(interestRates);
 lastDate = data(1,end);
@@ -23,4 +24,32 @@ for i = 1:T
     end
 end
 SP500_date_prices_returns_realizedvariance_interestRates = data;
-save('SP500_date_prices_returns_realizedvariance_interestRates_090320.mat', 'SP500_date_prices_returns_realizedvariance_interestRates');
+save('SP500_date_prices_returns_realizedvariance_interestRatesYield_090320.mat', 'SP500_date_prices_returns_realizedvariance_interestRates');
+
+%%
+clear;
+load('SP500_date_prices_returns_realizedvariance_090320.mat');
+data = SP500_date_prices_returns_realizedvariance_090320;
+
+load('interestRatesTbill2001to032020.mat');
+data(5:9,:) = NaN;
+[~, T] = size(interestRates);
+lastDate = data(1,end);
+for i = 1:T
+    if interestRates(1, i) <= lastDate
+        if i == 29
+            aa=0;
+        end
+        ind = find(data(1,:) == interestRates(1, i));
+        
+        if ~isempty(ind)
+            data(5:9, ind) = interestRates(2:end,i);
+            indtemp = ind;
+        else
+            data(5:9, indtemp + 1) = data(5:9, indtemp);
+            indtemp = indtemp + 1;
+        end
+    end
+end
+SP500_date_prices_returns_realizedvariance_interestRates = data;
+save('SP500_date_prices_returns_realizedvariance_interestRatesTbill_090320.mat', 'SP500_date_prices_returns_realizedvariance_interestRates');
